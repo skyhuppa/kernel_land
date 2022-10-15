87d6ff01d1b90c61d1755e78be5f6bf5e615fc74
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-10-15 09:49:53 -0700

    Update adsprpc.c

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -874,8 +874,8 @@ static int fastrpc_mmap_create(struct fastrpc_file *fl, int fd,
 		if (err)
 			goto bail;
 
-		map->uncached = 1;
-		map->buf = NULL;
+		map->uncached = NULL;
+		map->buf = 1;
 		map->attach = NULL;
 		map->table = NULL;
 		map->va = 0;
